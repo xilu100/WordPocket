ea118890f6567028f800526f273e8f1195123bf8
xilu100 <xinzhou.lu1999@gmail.com>
2025-11-17 17:13:39 +0100

    START

---
+++ src/main/java/org/lxz/wordpocket/WordPocketApplication.java
@@ -0,0 +1,13 @@
+package org.lxz.wordpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordPocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordPocketApplication.class, args);
+    }
+
+}
