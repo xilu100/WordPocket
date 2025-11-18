7facf3ce5fd2e117e246982335ece6017c987958
xilu100 <xilu100@hhu.de>
2025-11-18 16:16:33 +0100

    Rename some attributes name

--- src/main/java/org/lxz/wordpocket/domain/model/Properties.java
+++ src/main/java/org/lxz/wordpocket/domain/model/Properties.java
@@ -11,6 +11,6 @@ public class Properties {
 
     private List<Explanation> explanations;     //解释（词性，翻译，例句）
     private Language language;                  // 语种（英，德，法....）
-    private Set<String> tag;                   // 标签
-    private String explanation;                 // 补充说明（全称，完整意思，记忆方法等）
+    private Set<String> tags;                   // 标签
+    private String note;                 // 补充说明（全称，完整意思，记忆方法等）
 }
